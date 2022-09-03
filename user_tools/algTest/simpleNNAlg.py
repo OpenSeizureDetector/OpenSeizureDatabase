2ccb6439a6a459fb16e5a276fc5b883464dc5da9
Graham Jones <grahamjones139@gmail.com>
2022-09-03 14:44:49 +0100

    Disabled tensorflow so it will run on my 8GB Ram Laptop :)

---
+++ user_tools/algTest/simpleNNAlg.py
@@ -0,0 +1,70 @@
+#!usr/bin/python3
+
+"""
+A seizure detection algorithm based on a simple example neural network.
+
+Jamie Pordoy, 2022
+"""
+
+import json
+import numpy as np
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+import sdAlg
+
+class SimpleNNAlg(sdAlg.SdAlg):
+    def __init__(self, settingsStr, debug=False):
+        print("SimpleNNAlg.__init__() - settingsStr=%s" % settingsStr)
+        print("simpleNNA.__init__(): settingsStr=%s (%s)"
+                               % (settingsStr, type(settingsStr)))
+        super().__init__(settingsStr, debug)
+
+        modelFname = self.settingsObj['modelFile']
+        print("Loading Model %s..." % modelFname)
+        //self.model = pickle.load(open(modelFname,'rb'))
+
+
+        
+
+    def getAlarmState(self, rawData):
+        ''' getAlarmState(rawData) - determines the alarm state associated with the snapshot
+        of raw seizure detector data rawData[]
+         @return the alarm state (0=ok, 1 = alarm)
+        '''
+        if (self.DEBUG): print("FIXME - implement getAlarmState by evaluating neural network for given data")
+        inputVector = []
+
+        inputVector.append(rawData['roiPower'])
+        
+        print("inputVector=",inputVector)
+        alarmState = 0
+        //alarmState = self.model.predict(inputVector)
+        print("alarmState=",alarmState)
+        return(alarmState)
+        
+    def processDp(self, dpStr):
+        #print(dpStr)
+        inAlarm = self.getAlarmState(json.loads(dpStr))
+        
+        extraData = {
+            'alarmCount': 0,
+            'alarmState': inAlarm,
+            }
+        return json.dumps(extraData)
+        
+    def resetAlg(self):
+        self.alarmState = 0
+        self.alarmCount = 0
+
+                  
+if __name__ == "__main__":
+    print("osdAlg.OsdAlg.main()")
+    settingsObj = {
+        "alarmFreqMin" : 3,
+        "alarmFreqMax" : 8,
+        "alarmThreshold" : 100,
+        "alarmRatioThreshold" : 57
+        }
+    alg = LstmAlg(json.dumps(settingsObj),True)
