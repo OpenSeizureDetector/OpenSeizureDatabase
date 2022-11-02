79b0d99abb524645ab127385e1d2d8a02f58ada0
Graham Jones <grahamjones139@gmail.com>
2022-11-02 20:31:47 +0000

    Separated fft calculations into separate functions in osdAlgTools so we can use it in other modules.

--- libosd/osdAlgTools.py
+++ libosd/osdAlgTools.py
@@ -13,19 +13,25 @@ def getMagnitude(cVal):
     return(sumSq)
 
 
-
 def freq2fftBin(freq, freqRes):
     n = int(freq / freqRes)
     return(n)
 
+def getFreqRes(accData, sampleFreq=25.):
+    samplePeriod = len(accData)/sampleFreq
+    freqRes = 1.0 / samplePeriod
+    return(freqRes)
+
+def getFFT(accData, sampleFreq=25):
+    fftArr = np.fft.fft(accData)
+    fftFreq = np.fft.fftfreq(fftArr.shape[-1], 1.0/sampleFreq)
+    return(fftArr, fftFreq)
+
 def getSpecPower(accData, sampleFreq=25, freqCutoff=12.5, plotData = False):
     nSamp = len(accData)
     nFreqCutoff = freq2fftBin(freqCutoff)
-    fftArr = np.fft.fft(accData)
-    fftFreq = np.fft.fftfreq(fftArr.shape[-1], 1.0/sampleFreq)
-
+    fftArr, fftFreq = getFFT(accData, sampleFreq)
     specPower = 0.
-
     # range starts at 1 to avoid the DC component
     for i in range(1,nSamp):
         if (i<=nFreqCutoff):
@@ -34,15 +40,13 @@ def getSpecPower(accData, sampleFreq=25, freqCutoff=12.5, plotData = False):
     specPower = specPower / nSamp / 2
     return specPower
 
+
 def getRoiPower(accData, sampleFreq=25, alarmFreqMin = 3, alarmFreqMax=8,
                 plotData = False):
     nMin = freq2fftBin(alarmFreqMin)
     nMax = freq2fftBin(alarmFreqMax)
-    fftArr = np.fft.fft(accData)
-    fftFreq = np.fft.fftfreq(fftArr.shape[-1], 1.0/sampleFreq)
-
+    fftArr, fftFreq = getFFT(accData, sampleFreq)
     roiPower = 0.
-
     for i in range(nMin, nMax):
         roiPower = roiPower + getMagnitude(fftArr[i])
     roiPower = roiPower / (nMax - nMin)
