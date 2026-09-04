9afadfe6be22c6926c9d9e40785384706afb4de9
Graham Jones <grahamjones139@gmail.com>
2026-09-04 20:14:46 +0100

    More module search path fixes.

--- user_tools/testRunner/__init__.py
+++ user_tools/testRunner/__init__.py
@@ -1,9 +1,9 @@
 """testRunner package – public API re-exports."""
-from io_utils import loadDataFiles, load_training_event_ids, exclude_training_events_from_osd
-from output_folders import getOutputPath
-from alg_runner import testEachEvent
-from results import saveResults2, OTHERS_INDEX, ALL_INDEX, FALSE_INDEX, NDA_INDEX
-from report import generateSummaryReport, analyzeExistingResults
+from .io_utils import loadDataFiles, load_training_event_ids, exclude_training_events_from_osd
+from .output_folders import getOutputPath
+from .alg_runner import testEachEvent
+from .results import saveResults2, OTHERS_INDEX, ALL_INDEX, FALSE_INDEX, NDA_INDEX
+from .report import generateSummaryReport, analyzeExistingResults
 
 __all__ = [
     'loadDataFiles', 'load_training_event_ids', 'exclude_training_events_from_osd',
